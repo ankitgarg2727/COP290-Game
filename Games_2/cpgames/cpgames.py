5b1cc6ff777e34d5246e4dc36422d5c994a1480c
Irfan Hussain <142247827+kite029@users.noreply.github.com>
2024-04-17 16:50:14 +0530

    Update cpgames.py
    Changed in cpgames removed catchcoins extra initialization

--- Games_2/cpgames/cpgames.py
+++ Games_2/cpgames/cpgames.py
@@ -7,18 +7,13 @@ else:
 warnings.filterwarnings('ignore')
 
 class CPGames():
-    def __init__(self, **kwargs):
-        for key, value in kwargs.items(): setattr(self, key, value)
-        self.supported_games = self.initialize()
-    def execute(self, config={}):
-        client = self.supported_games["catchcoins"](**config)
-        client.run()
-    def initialize(self):
+    def __init__(self):
         supported_games = {
             'catchcoins': CatchCoinsGame,    
         }
-        return supported_games
-
+    def execute(self, config={}):
+        client = self.supported_games["catchcoins"](**config)
+        client.run()
 if __name__ == '__main__':
     games_client = CPGames()
     games_client.execute()
